d524d8dbf746f740f785974ec034e5a9025a75a8
BushSasha <sashabush2006@gmail.com>
2025-06-07 16:26:00 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,35 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <algorithm>
+
+class PMTreeNode {
+ public:
+    char symbol;
+    std::vector<PMTreeNode*> children;
+
+    PMTreeNode(char c);
+    ~PMTreeNode();
+};
+
+class PMTree {
+ private:
+    PMTreeNode* root;
+    int n;
+
+    void buildNode(PMTreeNode* parent, const std::vector<char>& elements);
+
+ public:
+    PMTree(const std::vector<char>& elements);
+    ~PMTree();
+
+    int getSize() const;
+    PMTreeNode* getRoot() const;
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
